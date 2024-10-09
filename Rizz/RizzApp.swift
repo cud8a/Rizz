ae0daef1a0de0a9ba5d94309a7dec4805f916d97
Tamas Bara <cudarammsch@gmail.com>
2024-10-09 18:45:06 +0200

    Initial Commit

---
+++ Rizz/RizzApp.swift
@@ -0,0 +1,17 @@
+//
+//  RizzApp.swift
+//  Rizz
+//
+//  Created by Tamas Bara on 09.10.24.
+//
+
+import SwiftUI
+
+@main
+struct RizzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
